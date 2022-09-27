d500aa149045265b2731aa40804af6c68476145d
Ha neul <skyhn0207@naver.com>
2022-09-27 22:20:40 +0900

    perfect

--- js/main.js
+++ js/main.js
@@ -1,33 +1,73 @@
-  <script>
-    
-    const list = document.querySelectorAll('.list');
+/* 마우스커서 */
+document.addEventListener('mousemove', function(e){
+  const dot = document.querySelector('.dot');
+  dot.style.left = e.pageX + 'px';
+  dot.style.top = e.pageY + 'px';
+});
 
-    function activeLink(){
-      list.forEach((item) => 
-      item.classList.remove('active'));
-      this.classList.add('active');
-    }
-    list.forEach((item) => item.addEventListener('click', activeLink));
+/* toggle_btn */
+let navigation = document.querySelector('.header_nav');
+let toggle = document.querySelector('.toggle');
+toggle.onclick = function(){
+  this.classList.toggle('active')
+  navigation.classList.toggle('noshow')
+}
 
-    
-    let navigation = document.querySelector('.navigation');
-    let toggle = document.querySelector('.toggle');
+/* portfolio */
+let plus = document.querySelector('.plus')
+let portfolio_menu = document.querySelector('.portfolio_menu')
+plus.onclick = function(){
+  portfolio_menu.classList.toggle('active')
+};
 
-    toggle.onclick = function(){
-    this.classList.toggle('active')
-    }
-    
-    $(document).ready(function(){
-      $('.toggle').click(function(){
-        $('.navigation').slideToggle(500);
-      })
+/* 모바일ver swiper */
+var swiper = new Swiper(".mobile", {
+  effect: "coverflow",
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 1,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+/* 마우스 스크롤 이벤트 */
+$(function(){
+  var $menu = $('.list');
+  var $content = $('main > section');
+  $menu.click(function(x){
+    x.preventDefault();
+    /* $(this).addClass('on').siblings().removeClass('on'); */
+    var idx = $(this).index();
+    var section = $content.eq(idx)
+    var sectionDistance = section.offset().top;
+    $('html,body').stop().animate({scrollTop:sectionDistance})
+  });
+
+  $(window).scroll(function(){
+    $content.each(function(){
+      if($(this).offset().top <= $(window).scrollTop()+10){
+        var idx = $(this).index();
+        $menu.removeClass('on');
+        $menu.eq(idx).addClass('on');
+      }
     })
+  })
 
-    
-    let plus = document.querySelector('.plus')
-    let portfolio_menu = document.querySelector('.portfolio_menu')
+  /* 팝업창 */
+  $('.pop_close').click(function(){
+    $('.popup').hide();
+    $('.popup_cover').hide();
+  })
+});
 
-    plus.onclick = function(){
-      portfolio_menu.classList.toggle('active')
-    }
-  </script>
+/* AOS 라이브러리 */
+AOS.init();
